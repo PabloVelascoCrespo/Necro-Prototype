6aebe153895b4a200d5cc39b05f2b28df214a4ed
PabloVelascoCrespo <72821847+PabloVelascoCrespo@users.noreply.github.com>
2025-02-03 17:58:32 +0100

    Empty project

---
+++ NecroPrototype/Source/NecroPrototype/NecroPrototype.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NecroPrototype.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NecroPrototype, "NecroPrototype" );
