c16ed0eaa0ee87a59d2d1ddc5b17d384f7cd4efe
Pablo Velasco Crespo <pablovc2000@gmail.com>
2025-02-22 11:08:53 +0100

    last commit

---
+++ NecroPrototype/Source/NecroProrotype/NecroPrototype.cpp
@@ -0,0 +1,7 @@
+
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NecroPrototype.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NecroPrototype, "NecroPrototype" );
